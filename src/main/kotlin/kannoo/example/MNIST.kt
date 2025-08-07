04227f48efe8a416ea3301f3a76d4a2707084569
Luc Ruijsink <lruijsink@gmail.com>
2025-08-08 00:34:00 +0200

    Better MNIST error display

--- src/main/kotlin/kannoo/example/MNIST.kt
+++ src/main/kotlin/kannoo/example/MNIST.kt
@@ -1,5 +1,6 @@
 package kannoo.example
 
+import kannoo.core.CostFunction
 import kannoo.core.InputLayer
 import kannoo.core.Model
 import kannoo.core.Sample
@@ -45,6 +46,37 @@ fun readCSVs(fileName: String): List<Sample> =
         .map { it.split(',') }
         .map { ex -> Sample(input = inputOf(ex.drop(1)), target = targetOf(ex[0])) }
 
+fun showTestSetError(testSet: List<Sample>, model: Model, cost: CostFunction, compact: Boolean = false) {
+    val count = MutableList(10) { 0 }
+    val costSum = MutableList(10) { 0.0 }
+    val mseSum = MutableList(10) { 0.0 }
+    val outputSum = MutableList(10) { Vector(10) }
+
+    testSet.forEach { (input, target) ->
+        val digit = (0..9).first { n -> target[n] == 1.0 }
+        val output = model.compute(input)
+        count[digit]++
+        outputSum[digit].plusAssign(output)
+        costSum[digit] += cost.compute(target, output)
+        mseSum[digit] += MeanSquaredError.compute(target, output)
+    }
+
+    println(
+        "Mean error: " + rnd(costSum.sum() / testSet.size) +
+                "  MSE: " + rnd(mseSum.sum() / testSet.size),
+    )
+    if (!compact) {
+        (0..9).forEach { digit ->
+            println(
+                "   $digit " +
+                        "error: ${rnd(costSum[digit] / count[digit].toDouble())}" +
+                        "    " +
+                        "mean: ${rnd(outputSum[digit][digit] / count[digit].toDouble())}",
+            )
+        }
+    }
+}
+
 fun MNIST() {
     val trainingFile = "./data/mnist_train.csv"
     val testFile = "./data/mnist_test.csv"
@@ -53,15 +85,15 @@ fun MNIST() {
     val trainingSet = readCSVs(trainingFile)
 
     println("Parsing test set...")
-    val testSet = readCSVs(testFile)
-    val miniTestSet = testSet.shuffled().take(1000)
+    val fullTestSet = readCSVs(testFile)
+    val miniTestSet = fullTestSet.shuffled().take(1000)
 
     val cost = CrossEntropyLoss
     val model = try {
         readModelFromFile(MNIST_MODEL_FILE)
             .also { println("Successfully loaded pre-trained model") }
     } catch (e: Exception) {
-        println("Pre-trained model not found, creating new instance (${e})")
+        println("Pre-trained model not found, creating new instance ($e)")
         Model(
             InputLayer(28 * 28),
             DenseLayer(256, ReLU),
@@ -69,46 +101,28 @@ fun MNIST() {
             DenseLayer(10, Softmax),
         )
     }
-    val sgd = MiniBatchSGD(model, cost, 10, 0.1)
+    val sgd = MiniBatchSGD(model, cost, 64, 0.1)
 
     (1..100).forEach { n ->
         val perRound = 6000
 
+        println()
+        println("=====================================")
+        println("               ROUND $n")
+        println()
+        println("Calculating error over full test set:")
+        println()
+        showTestSetError(fullTestSet, model, cost)
+        println()
+
         trainingSet.shuffled().chunked(perRound).forEachIndexed { i, subSet ->
-            println("")
-            println("================================")
-            println("")
 
             println("Training round $n, subset ${i + 1} / ${trainingSet.size / perRound}")
             sgd.apply(subSet)
             writeModelToFile(model, MNIST_MODEL_FILE)
 
-            val count = MutableList(10) { 0 }
-            val costSum = MutableList(10) { 0.0 }
-            val mseSum = MutableList(10) { 0.0 }
-            val outputSum = MutableList(10) { Vector(10) }
-
-            miniTestSet.forEach { (input, target) ->
-                val digit = (0..9).first { n -> target[n] == 1.0 }
-                val output = model.compute(input)
-                count[digit]++
-                outputSum[digit].plusAssign(output)
-                costSum[digit] += cost.compute(target, output)
-                mseSum[digit] += MeanSquaredError.compute(target, output)
-            }
-
-            println(
-                "Mean error: " + rnd(costSum.sum() / miniTestSet.size) +
-                        "  MSE: " + rnd(mseSum.sum() / miniTestSet.size),
-            )
-            (0..9).forEach { digit ->
-                println(
-                    "   $digit " +
-                            "error: ${rnd(costSum[digit] / count[digit].toDouble())}" +
-                            "    " +
-                            "mean: ${rnd(outputSum[digit][digit] / count[digit].toDouble())}",
-                )
-            }
+            showTestSetError(miniTestSet, model, cost, true)
+            println()
         }
     }
 }
