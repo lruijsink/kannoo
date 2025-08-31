46b4ddb2b28e5364935541b4e843e78ebc9a5799
Luc Ruijsink <lruijsink@gmail.com>
2025-08-31 21:51:35 +0200

    Fix simple shader, make CPU/GPU ratio variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,23 +8,22 @@ import kannoo.vulkan.Vulkan
 import kannoo.vulkan.VulkanMatrixMultiply
 import kotlin.system.measureTimeMillis
 
+val simpleShader = Shader(
+    fileName = "shaders/simple.spv",
+    workgroupSize = 32,
+)
+
+val tiledShader = Shader(
+    fileName = "shaders/tiled.spv",
+    workgroupSize = 32,
+)
+
 fun main() {
-    val R = 1000
-    val inputSize = 640
-    val outputSize = 1280
-    val batchSize = 1280
+    val inputSize = 2048
+    val outputSize = 1024
+    val batchSize = 512
     val input = randomMatrix(batchSize, inputSize)
     val weights = randomMatrix(outputSize, inputSize)
-
-    val simpleShader = Shader(
-        fileName = "shaders/simple.spv",
-        workgroupSize = 32,
-    )
-
-    val tiledShader = Shader(
-        fileName = "shaders/tiled.spv",
-        workgroupSize = 32,
-    )
 
     println("Creating Vulkan instance")
     val vulkan = Vulkan()
@@ -40,6 +39,9 @@ fun main() {
     )
     println("Matrix multiply pipeline successfully created")
 
+    val rounds = 1000
+    val cpuReduction = 100
+
     println()
     println("                         batches   input/output vectors")
     println("                              |     |")
@@ -48,30 +50,30 @@ fun main() {
     println("Weights matrix dimensions: ${weights.shape}")
     println("Output matrix dimensions:   ${Shape(batchSize, outputSize)} = Input * Weights.transpose()")
     println()
-    val calc = R.toLong() * batchSize * inputSize * outputSize
-    println("Total work = $R repetitions * $batchSize batches * $inputSize input vectors * $outputSize weights per input")
+    val calc = rounds.toLong() * batchSize * inputSize * outputSize
+    println("Total work = $rounds repetitions * $batchSize batches * $inputSize input vectors * $outputSize weights per input")
     println("           = $calc (${rnd(calc / 1_000_000_000_000.0f)} trillion) calculations")
     println()
 
     var ref: Matrix? = null
     val cpuMs = measureTimeMillis {
-        repeat(R / 100) {
+        repeat(rounds / cpuReduction) {
             ref = Matrix(Array(batchSize) { i -> weights * input[i] })
         }
     }
     ref!!
-    println("CPU took ${rnd(cpuMs / 1000.0f)} sec. for ${R / 100}   matrix multiplications (${R.toFloat() * 10 / cpuMs} mmuls/sec.)")
+    println("CPU took ${rnd(cpuMs / 1000.0f)} sec. for ${rounds / cpuReduction}   matrix multiplications (${rounds.toFloat() * 1000 / (cpuMs * cpuReduction)} mmuls/sec.)")
     println()
 
     val output = Matrix(batchSize, outputSize)
     val runsMs = measureTimeMillis {
-        repeat(R) {
+        repeat(rounds) {
             vulkanMatrixMultiply.multiplyTransposed(input, weights, output)
         }
     }
-    println("GPU took  ${rnd(runsMs / 1000.0f)} sec. for $R matrix multiplications (${R.toFloat() * 1000 / runsMs} mmuls/sec.)")
+    println("GPU took  ${rnd(runsMs / 1000.0f)} sec. for $rounds matrix multiplications (${rounds.toFloat() * 1000 / runsMs} mmuls/sec.)")
     println()
-    println("GPU is ${rnd((cpuMs.toFloat() / runsMs) * 100)} times faster")
+    println("GPU is ${rnd((cpuMs.toFloat() / runsMs) * cpuReduction)} times faster")
     println()
 
     println("Destroying Vulkan instance")
